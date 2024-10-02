c15ab20b7ae3f4e1a86540f859192e6b6db3a109
Rachel <racheljay42@gmail.com>
2024-10-01 17:25:32 -0700

    timer functionality

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,12 +1,42 @@
-const button = document.querySelector("button")
-const readout = document.querySelector("p")
+const action = document.querySelector("#action")
+const readout = document.querySelector("#clickCount")
+const stop = document.querySelector("#stop")
+const timer = document.querySelector("#timer")
+const time = document.querySelector("#time")
+
+let nIntervId
 
 let count = 0
 
+timeRecordings = []
+
 function click() {
-    console.log("click")
     count++
-    readout.textContent = count
+    clickCount.textContent = count
+    timeRecordings.push(timeStart)
+    console.log(timeRecordings)
 }
 
-button.addEventListener("click", () => click())
+let timeStart = 5
+
+function addTime() {
+    timeStart--
+    time.textContent = timeStart
+}
+
+function startTimer() {
+    nIntervId = setInterval(addTime, 1000)
+}
+
+function stopTime() {
+    clearInterval(nIntervId)
+    time.textContent = 5
+    timeStart = 5
+    count = 0
+}
+
+
+
+action.addEventListener("click", click)
+timer.addEventListener("click", startTimer)
+stop.addEventListener("click", stopTime)
